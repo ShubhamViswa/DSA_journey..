e475d89126c18b9821ffe0253d71916667479795
Shubham Vishwakarma <shubhamviswa2000@gmail.com>
2024-05-17 15:10:42 +0530

    Create twoSum.js

---
+++ 1. Arrays/twoSum.js
@@ -0,0 +1,15 @@
+// Question link --> https://leetcode.com/problems/two-sum/description/
+
+let twoSum = function(nums, target){
+    let n=nums.length
+    for(let i = 0; i < n-1; i++){
+    for(let j = i + 1; j<n ; j++){
+        if(nums[i]+nums[j] == target){
+            return [i,j];
+        }
+    }
+  }
+  return null; 
+}
+
+console.log(twoSum([4,66,2,66,],132));
